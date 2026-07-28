d6c863a50cb9fc700f0c0751fdb325750143ba28
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-07-28 14:20:04 -0400

    Add least-privilege control-plane IAM inputs

---
+++ infra/terraform/modules/iam/variables.tf
@@ -0,0 +1,40 @@
+variable "name" {
+  type = string
+}
+
+variable "execution_queue_arn" {
+  type = string
+}
+
+variable "platform_bucket_arn" {
+  type = string
+}
+
+variable "execution_bucket_arn" {
+  type = string
+}
+
+variable "platform_kms_key_arn" {
+  type = string
+}
+
+variable "execution_kms_key_arn" {
+  type = string
+}
+
+variable "runtime_secret_arns" {
+  description = "Secrets ECS may inject at task startup; candidate pods never receive these permissions."
+  type        = set(string)
+  default     = []
+}
+
+variable "execution_cluster_arn" {
+  type     = string
+  default  = null
+  nullable = true
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
