75a49d725070e9bb7300cf64b1c27270ebd70a7e
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-07-28 23:08:24 -0400

    Format staging execution queue Terraform

--- infra/terraform/environments/staging/execution_queue.tf
+++ infra/terraform/environments/staging/execution_queue.tf
@@ -1,7 +1,7 @@
 module "execution_queue" {
   source = "../../modules/execution_queue"
 
-  name_prefix               = var.name_prefix
+  name_prefix                = var.name_prefix
   visibility_timeout_seconds = 120
   message_retention_seconds  = 345600
   dlq_retention_seconds      = 1209600
