e87ccad6a7fb23c42aef4d3c294578da6cc0c786
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-08-26 13:01:01 -0400

    Format SkillForge prod Terraform

--- infra/terraform/environments/prod/main.tf
+++ infra/terraform/environments/prod/main.tf
@@ -56,27 +56,27 @@ module "application" {
   vpc_id               = module.network.vpc_id
   public_subnet_ids    = module.network.public_subnet_ids
   private_subnet_ids   = module.network.private_subnet_ids
-  api_image             = var.api_image
-  worker_image          = var.worker_image
-  certificate_arn       = module.certificate.certificate_arn
-  execution_queue_arn   = module.execution_queue.queue_arn
-  background_queue_arn  = module.background_queue.queue_arn
-  upload_bucket_arn     = module.storage.upload_bucket_arn
-  export_bucket_arn     = module.storage.export_bucket_arn
+  api_image            = var.api_image
+  worker_image         = var.worker_image
+  certificate_arn      = module.certificate.certificate_arn
+  execution_queue_arn  = module.execution_queue.queue_arn
+  background_queue_arn = module.background_queue.queue_arn
+  upload_bucket_arn    = module.storage.upload_bucket_arn
+  export_bucket_arn    = module.storage.export_bucket_arn
 
   environment = {
-    RIGOR_ENVIRONMENT             = "production"
-    RIGOR_LOCAL_OIDC_ENABLED      = "false"
-    RIGOR_EXECUTION_ADAPTER       = "KUBERNETES_JOB"
-    RIGOR_AI_ADAPTER              = "DETERMINISTIC"
-    CLERK_ISSUER                  = var.clerk_issuer
-    CLERK_JWKS_URL                = var.clerk_jwks_url
-    JWT_AUDIENCE                  = var.jwt_audience
-    AWS_REGION                    = var.aws_region
-    SQS_EXECUTION_QUEUE_URL       = module.execution_queue.queue_url
-    RIGOR_BACKGROUND_QUEUE_URL    = module.background_queue.queue_url
-    S3_UPLOAD_BUCKET              = module.storage.upload_bucket_name
-    S3_EXPORT_BUCKET              = module.storage.export_bucket_name
+    RIGOR_ENVIRONMENT          = "production"
+    RIGOR_LOCAL_OIDC_ENABLED   = "false"
+    RIGOR_EXECUTION_ADAPTER    = "KUBERNETES_JOB"
+    RIGOR_AI_ADAPTER           = "DETERMINISTIC"
+    CLERK_ISSUER               = var.clerk_issuer
+    CLERK_JWKS_URL             = var.clerk_jwks_url
+    JWT_AUDIENCE               = var.jwt_audience
+    AWS_REGION                 = var.aws_region
+    SQS_EXECUTION_QUEUE_URL    = module.execution_queue.queue_url
+    RIGOR_BACKGROUND_QUEUE_URL = module.background_queue.queue_url
+    S3_UPLOAD_BUCKET           = module.storage.upload_bucket_name
+    S3_EXPORT_BUCKET           = module.storage.export_bucket_name
   }
 
   secret_arns = module.secrets.secret_arns
