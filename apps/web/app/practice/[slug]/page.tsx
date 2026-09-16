321305e08baeb02977e67b1c2ee4ca61c7d0086d
rrahul0904 <rrahul0904@users.noreply.github.com>
2026-09-16 02:04:06 -0400

    feat: use guided practice experience

--- apps/web/app/practice/[slug]/page.tsx
+++ apps/web/app/practice/[slug]/page.tsx
@@ -1,5 +1,4 @@
-import { SkillsForgePracticeWorkspace } from "@/components/skillsforge-practice-workspace";
-import { TutorDock } from "@/components/tutor-dock";
+import { GuidedPracticeExperience } from "@/components/guided-practice-experience";
 
 export default async function PracticePage({
   params,
@@ -7,10 +6,5 @@ export default async function PracticePage({
   params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
-  return (
-    <>
-      <SkillsForgePracticeWorkspace slug={slug} />
-      <TutorDock slug={slug} />
-    </>
-  );
+  return <GuidedPracticeExperience slug={slug} />;
 }
